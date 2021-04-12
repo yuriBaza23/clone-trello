b954d5a67b8bbf926c89e976d09ff85efd965381
yuriBaza23 <yuribaza2321@gmail.com>
2021-04-12 02:20:17 -0300

    Create Board Button component

---
+++ src/components/CreateBoardButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const CreateBoardButton: React.FC = () => {
+    return (
+        <button className="create-board-button-component">
+            Criar novo quadro
+        </button>
+    );
+}
+
+export default CreateBoardButton;
