259695eaf078687d20f2212635c313b0b4a2df41
yuriBaza23 <yuribaza2321@gmail.com>
2021-04-12 15:26:37 -0300

    Add Mini Cards

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiChevronDown, FiClock, FiHome, FiPlus, FiStar, FiTrello } from 'react-icons/fi';
+import { FiChevronDown, FiClock, FiHome, FiPlus, FiSettings, FiStar, FiTrello, FiUser } from 'react-icons/fi';
+import { CgViewList } from 'react-icons/cg';
 import Button from '../../components/Button';
 import CreateBoardButton from '../../components/CreateBoardButton';
 import Header from '../../components/Header';
@@ -103,7 +104,69 @@ const Boards: React.FC = () => {
                             <h1>A</h1>
                         </div>
                         <h1>AWS</h1>
-                        <Button/>
+                        <Button text='Quadros'>
+                            <FiTrello/>
+                        </Button>
+                        <Button text='Tabela da Área de trabalho'>
+                            <CgViewList/>
+                        </Button>
+                        <Button text='Membros (210)'>
+                            <FiUser/>
+                        </Button>
+                        <Button text='Configurações'>
+                            <FiSettings/>
+                        </Button>
+                        <Button text='Atualizar' brief>
+                            <FiSettings/>
+                        </Button>
+                    </div>
+                    <div className="row-cards">
+                        <MiniCard
+                            name='Cloud'
+                            time='AWS'
+                            color='red'
+                        />
+                        <CreateBoardButton/>
+                    </div>
+
+                    <div className="destaque-name">
+                        <div className="init-letter">
+                            <h1>A</h1>
+                        </div>
+                        <h1>Apl...</h1>
+                        <Button text='Quadros'>
+                            <FiTrello/>
+                        </Button>
+                        <Button text='Tabela da Área de trabalho'>
+                            <CgViewList/>
+                        </Button>
+                        <Button text='Membros (210)'>
+                            <FiUser/>
+                        </Button>
+                        <Button text='Configurações'>
+                            <FiSettings/>
+                        </Button>
+                        <Button text='Atualizar' brief>
+                            <FiSettings/>
+                        </Button>
+                    </div>
+                    <div className="row-cards">
+                        <MiniCard
+                            name='Technology'
+                            time='Apple'
+                            color='blue'
+                            fav
+                        />
+                        <MiniCard
+                            name='Design'
+                            time='Apple'
+                            color='green'
+                        />
+                        <CreateBoardButton/>
+                    </div>
+
+                    <div className="destaque-name">
+                        <Button text='Ver todos os quadros fechados'/>
                     </div>
                 </div>
             </div>
