e0799dedf5ac90db5faf8dcda7ea1a6048e8d30c
yuriBaza23 <yuribaza2321@gmail.com>
2021-04-12 15:25:18 -0300

    Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { FiBriefcase } from 'react-icons/fi';
 import './styles.css';
 
-const Button: React.FC = () => {
-  return <div />;
+interface IButtonProps {
+  text: string;
+  brief?: boolean;
+}
+
+const Button: React.FC<IButtonProps> = ({ children, text, brief }) => {
+  return(
+    <button className={`button-component ${brief && 'brief'}`}>
+      { brief ? (
+        <div className="brief-case">
+          <FiBriefcase/>
+        </div>
+      ) : children }
+      <span>{text}</span>
+    </button>
+  );
 }
 
 export default Button;
